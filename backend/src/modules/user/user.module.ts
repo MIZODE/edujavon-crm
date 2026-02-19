ac1f71e4db74840a603e5f4c2c9b490b58bca563
saferboy <muxtordev@gmail.com>
2026-02-19 13:03:35 +0500

    add role permission

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,14 +1,46 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
 import { authenticate } from "../../common/middleware/auth.middleware";
+import { rolePermission } from "../../common/middleware/role.middleware";
+import { Role } from "../../../generated/prisma/client";
 
 export const userRouter = Router();
 
-userRouter.get("/users", authenticate, userController.getAll);
-userRouter.get("/:id", userController.getOne);
-userRouter.post("/", userController.create);
-userRouter.patch("/up/:id", userController.update);
-userRouter.delete('/hard/:id', userController.hardDelete);
-userRouter.put('/soft/:id', userController.softDelete);
+userRouter.get(
+    "/users",
+    authenticate,
+    rolePermission([Role.ADMIN, Role.OWNER, Role.SUPERVISOR]),
+    userController.getAll
+);
+
+userRouter.get(
+    "/:id",
+    authenticate,
+    userController.getOne
+);
+
+userRouter.post(
+    "/",
+    authenticate,
+    userController.create
+);
+
+userRouter.patch(
+    "/up/:id",
+    authenticate,
+    userController.update
+);
+
+userRouter.delete(
+    "/hard/:id",
+    authenticate,
+    userController.hardDelete
+);
+
+userRouter.put(
+    "/soft/:id",
+    authenticate,
+    userController.softDelete
+);
 
 export default userRouter;
